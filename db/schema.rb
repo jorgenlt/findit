e76a0f6d1fb80ac24735540996196f96157f4fd8
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-02-21 01:31:20 +0800

    created table properties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_172311) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_172936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_172311) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "properties", force: :cascade do |t|
+    t.string "property_name"
+    t.string "property_value"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_properties_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,4 +64,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_172311) do
 
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
+  add_foreign_key "properties", "categories"
 end
