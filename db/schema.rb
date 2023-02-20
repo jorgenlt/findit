9216a34d49992ef0c32771c67b8c01a31d9fb1a4
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-02-21 01:55:01 +0800

    created junction table for category and property

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_172936) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_175220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_172936) do
     t.integer "parent_category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "category_properties", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_properties_on_category_id"
+    t.index ["property_id"], name: "index_category_properties_on_property_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,10 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_172936) do
   create_table "properties", force: :cascade do |t|
     t.string "property_name"
     t.string "property_value"
-    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_properties_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,7 +69,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_172936) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "category_properties", "categories"
+  add_foreign_key "category_properties", "properties"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
-  add_foreign_key "properties", "categories"
 end
