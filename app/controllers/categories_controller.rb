97909a8a963109ae66cfad850a11e464d487ca8f
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-02-25 10:29:55 +0100

    cat and subcat

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,14 @@
 class CategoriesController < ApplicationController
 
   def show
+    # raise
     @category = Category.find(params[:id])
     @sub_categories = Category.where(parent_category_id: params[:id])
   end
 
   def show_sub
-    raise
-    # @sub = Category.find(params[:id])
+    # raise
+    @sub_category = Category.find(params[:category_id])
+    @parent_category = Category.find(params[:id])
   end
-
 end
