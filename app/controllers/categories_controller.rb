797c11764590fac4debbb228d9e750b3c849a793
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-02-26 21:36:52 +0100

    show and show_sub. grid view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,16 @@
 class CategoriesController < ApplicationController
 
   def show
-    # raise
     @category = Category.find(params[:id])
     @sub_categories = Category.where(parent_category_id: params[:id])
+    @posts = Post.where(category_id: params[:id])
   end
 
   def show_sub
-    # raise
+    # @parent_category = Category.find(params[:id])
     @sub_category = Category.find(params[:category_id])
-    @parent_category = Category.find(params[:id])
+    @category = Category.find(params[:id])
+    @sub_categories = Category.where(parent_category_id: params[:id])
     @posts = Post.where(category_id: params[:id], sub_category_id: params[:category_id])
   end
 end
