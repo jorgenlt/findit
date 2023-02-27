52d3dc52693d98be35fbd5edb71daf182ce35dfd
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-02-27 23:37:26 +0100

    search feature on dashboard

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  include PgSearch::Model
+  pg_search_scope :search_posts,
+    against: [ :post_title, :post_body ],
+      using: { tsearch: { prefix: true } }
 end
