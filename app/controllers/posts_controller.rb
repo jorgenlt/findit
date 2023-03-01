4bd2bc521a23d9eedf6c10f3a84574d864dcfc7b
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-03-01 13:38:32 +0100

    create new post with cat and subcat

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,22 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
+    # @categories = Category.where(parent_category_id: nil)
+
+    # @sub_categories = Category.where.not(parent_category_id: nil)
+
   end
 
+
+
+
+
   def create
+    category_id = Category.find_by(category_name: post_params[:category_id].downcase).id
+    sub_category_id = Category.find_by(category_name: post_params[:sub_category_id].downcase).id
     @post = Post.new(post_params)
+    @post.category_id = category_id
+    @post.sub_category_id = sub_category_id
     @post.user_id = current_user.id
     @post.save
     redirect_to root_path
